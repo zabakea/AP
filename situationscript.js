2b058d6bcd4d30d70209ecd9ebdda82721befead
Laura Vitali <lauravitali@MacBook-Pro-di-Laura.local>
2020-11-18 12:13:50 +0100

    fetching single situation description breaks everything :)

--- situationscript.js
+++ situationscript.js
@@ -62,15 +62,15 @@ function getData() {
     console.log(oneSitu_id);
 
     //fetch data
-    fetch(link)
-        .then(function (response) {
-            console.log(response);
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-            dataReceived(data);
-        })
+    if (oneSitu_id) {
+        fetch(link)
+            .then(res => res.json())
+            .then(showSituation)
+    } else {
+        fetch(link)
+            .then(res => res.json())
+            .then(dataReceived)
+    }
 }
 
 function dataReceived(situations) {
@@ -86,8 +86,15 @@ function showSituation(oneSituation) {
     const template = document.querySelector("#situation-template").content;
     const clone = template.cloneNode(true);
 
+    const situDescription = clone.querySelector("#situation-description");
+    if (situDescription) {
+        situDescription.innerHTML = oneSituation.excerpt.rendered;
+    }
+
     const a = clone.querySelector('a');
-    a.href += oneSituation.id;
+    if (a) {
+        a.href += oneSituation.id;
+    }
 
     //fill template
     clone.querySelector("#card-title").innerHTML = oneSituation.title.rendered;
